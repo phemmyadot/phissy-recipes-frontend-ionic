ee2a390240935ae7f8622c65896bbe270c1a7d3f
phemmyadot <50578808+phemmyadot@users.noreply.github.com>
2020-01-28 21:38:15 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080/graphql',
-  imageUploadUrl: 'http://localhost:8080/post-image'
+  baseUrl: 'https://phissy-node-app.herokuapp.com/graphql',
+  imageUploadUrl: 'https://phissy-node-app.herokuapp.com/post-image'
 };
 
 /*
