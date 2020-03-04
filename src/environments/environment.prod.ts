d28f65e05ce3e07afb22c1b63dfd30f825a71985
Babafemi Adojutelegan <badojutelegan@eminenttechnology.com>
2020-03-04 16:58:30 +0100

    updated environment var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: process.env.GRAPHQL_BASE_URL,
-  imageUploadUrl: process.env.UPLOAD_BASE_URL,
-  host: process.env.HOST_BASE_URL
+  baseUrl: `${process.env.GRAPHQL_BASE_URL}`,
+  imageUploadUrl: `${process.env.UPLOAD_BASE_URL}`,
+  host: `${process.env.HOST_BASE_URL}`
 };
